5fc01a22cb7a3024632477867c4bd398dc413e71
Max Miller <maxmiller413@gmail.com>
2021-01-15 00:25:47 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,17 +4,22 @@ def new_hash
 end
 
 def my_hash
-  {:favorite_color => "green"}
+  {
+    :key => "value"
+  }
   # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
-  {:name => "Grace Hopper"}
+  {
+    :name => "Grace Hopper"
+  }
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
-
 def id_hash_generator(number)
-  {:id => number} 
+  {
+    :id => number
+  }
   # return a hash with a key :id assigned to the provided number
 end
